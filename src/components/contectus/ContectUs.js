b10a17d00b63387d0849d68a8ba2da1f1170e6f1
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-09-27 00:39:02 -0700

    adding contact form data and CV info

--- src/components/contectus/ContectUs.js
+++ src/components/contectus/ContectUs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Contectstyle.css'
+import AbuBakar from '../images/AbuBakar.pdf'
+import gitdata from '../images/gitdata.zip'
 
 
 export default function ContectUs(props) {
@@ -9,18 +11,18 @@ export default function ContectUs(props) {
                 <div className='row'>
                     <div className='col-lg-12 col-md-12 col-12'>
                         <h1 className='my-3' style={{color:props.mode==='dark'?'white':'black'}}>CONTACT WITH<span className='chanimations'> ME</span></h1>
-                        <form className='my-5'>
+                        <form action='https://formspree.io/f/xpzgyvgg' method='post' className='my-5'>
                             <div className="mb-3" id='divsize'>
                                 <label htmlFor="Username" className="form-label" id="colordata">Name</label>
-                                <input type="text" className="form-control" id="example" placeholder="NAME" />
+                                <input type="text" name='username' className="form-control" id="example" placeholder="NAME" required />
                             </div>
                             <div className="mb-3" id='divsize'>
-                                <label htmlFor="EnterEmail" className="form-label" id="colordata">Email address</label>
-                                <input type="email" className="form-control" id="exampleForm" placeholder="name@example.com" />
+                                <label  htmlFor="EnterEmail" className="form-label" id="colordata">Email address</label>
+                                <input type="email" name='email' className="form-control" id="exampleForm" placeholder="name@example.com" required />
                             </div>
                             <div className="mb-3" id='divsize'>
                                 <label htmlFor="Message" className="form-label" id="colordata">MESSAGE</label>
-                                <textarea className="form-control" id="exampleFormControl" rows="5" placeholder="MESSAGE"></textarea>
+                                <textarea name='message' className="form-control" id="exampleFormControl" cols="30" rows="6" placeholder="MESSAGE" required></textarea>
                             </div>
                             <button type="submit" className="btn" id='contectbtn'>SEND_INPUT</button>
                         </form>
@@ -32,9 +34,9 @@ export default function ContectUs(props) {
                     <div className='col-lg-12 col-md-12 col-12'>
                         <div className='d-flex'>
 
-                            <a href="#" className='btn btn-success' download="">DOWNLOAD_CV <i className="fa-solid fa-download"></i></a>
+                            <a href={AbuBakar} className='btn btn-success' download>DOWNLOAD_CV <i className="fa-solid fa-download"></i></a>
 
-                            <a href="#" className='btn btn-success mx-3'  download=" " >DOWNLOAD_NOTES <i className="fa-solid fa-download"></i></a>
+                            <a href={gitdata} className='btn btn-success mx-3'  download>DOWNLOAD_NOTES <i className="fa-solid fa-download"></i></a>
 
                         </div>
                     </div>
